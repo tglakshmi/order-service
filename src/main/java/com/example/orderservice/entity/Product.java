9312461ecc00296179485587ebc2b23b45f6625e
Kishore <kishore2603>
2024-02-28 17:29:38 -0600

    Updated location

---
+++ src/main/java/com/example/orderservice/entity/Product.java
@@ -0,0 +1,25 @@
+package com.example.orderservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Long id;
+    @Column(name="Product_Name")
+    private String productName;
+    @Column(name="Product_Title")
+    private String productTitle;
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="order_id")
+    private Order order;
+}
